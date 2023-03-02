c57bfcad43037366654968f95b268b9fd6e7981c
Nerdy Birdy <holliesundra@gmail.com>
2023-03-01 21:56:51 -0700

    Add to cart btn start

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,3 +1,5 @@
+const { doc } = require("prettier");
+
 const newFormHandler = async (event) => {
   event.preventDefault();
 
@@ -45,3 +47,37 @@ document
 document
   .querySelector('.project-list')
   .addEventListener('click', delButtonHandler);
+
+
+function addToCart() {
+  const productName = document.querySelector('.card-title').textContent.trim();
+  const productImg = document.querySelector('.card-img-top').getAttribute('src').trim();
+  const productDesc = document.querySelector('.card-text').textContent.trim();
+  const productPrice = document.querySelector('.card-price').textContent.trim();
+
+
+  const product = {
+    name: productName,
+    img: productImg,
+    description: productDesc,
+    price: productPrice,
+  };
+
+  const cart = [];
+
+  function addProductToCart(product) {
+    cart.push(product);
+    console.log(cart);
+  }
+
+  const addToCartBtn = document.querySelector('.btn-primary-add-to-cart');
+  addToCartBtn.addEventListener('click', function() {
+    event.preventDefault();
+    addProductToCart(product);
+  });
+
+  return product;
+
+}
+
+addToCart();
